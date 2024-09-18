41642267163b1dee47a99d9deb94fea5cd1e338c
Shyaka Chaste <liochastej@gmail.com>
2024-09-18 12:09:55 -0700

    updated

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 
 const Partners = () => {
   const partners = [
-    { id: 1, logo: '/assets/hackclub.png', alt: 'Partner 1' },
+    { id: 1, logo: '/assets/hackclub.png', alt: 'Partner 1' , link: 'https://hackclub.com' },
   ];
 
   return (
@@ -18,7 +18,9 @@ const Partners = () => {
             whileHover={{ scale: 1.1, rotate: 5 }}
             whileTap={{ scale: 0.9 }}
           >
-            <img src={partner.logo} alt={partner.alt} className="partner-logo" />
+            <a href={partner.link} target="_blank" rel="noopener noreferrer">
+              <img src={partner.logo} alt={partner.alt} className="partner-logo" />
+            </a>
           </motion.div>
         ))}
       </div>
