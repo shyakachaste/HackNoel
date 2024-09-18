713557fe44afffae0b45af61a9fc42c4490df755
Shyaka Chaste <liochastej@gmail.com>
2024-09-18 11:56:38 -0700

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="social-icons">
-        <a href="https://discord.com" target="_blank" rel="noopener noreferrer"><FaDiscord /></a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+        <a href="https://discord.com/invite/h5dfqzGZ22" target="_blank" rel="noopener noreferrer"><FaDiscord /></a>
+        <a href="https://www.instagram.com/youthcodecamp_rw/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+        <a href="https://www.linkedin.com/company/youth-code-camp/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+        <a href="https://github.com/Youth-codecamp" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
       </div>
       <p className="footer-text">
       Youth Code Camp is fiscally sponsored by The Hack Foundation (d.b.a. Hack Club), a 501(c)(3) nonprofit (EIN: 81-2908499). Copyright © 2024.
