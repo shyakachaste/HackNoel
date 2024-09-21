e4db4e13e125735561b0f25d2c2751492d5781ca
Shyaka Chaste <liochastej@gmail.com>
2024-09-21 15:56:33 -0700

    updated

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -7,12 +7,12 @@ const Schedule = () => {
     { time: '8:15 AM - 9:00 AM', description: 'Welcoming' },
     { time: '9:00 AM - 9:45 AM', description: 'Introducing HackNoel' },
     { time: '9:45 AM - 10:15 AM', description: 'Breakfast' },
-    { time: '10:15 AM - 1:15 PM', description: 'Working on Projects' },
+    { time: '10:15 AM - 1:15 AM', description: 'Working on Projects' },
     { time: '1:15 PM - 2:15 PM', description: 'Lunch' },
     { time: '2:15 PM - 3:00 PM', description: 'Guest of Honor Speech' },
-    { time: '3:00 PM - 6:00 PM', description: 'Working on Projects Part 2' },
-    { time: '6:00 PM - 7:30 PM', description: 'Dinner' },
-    { time: '7:30 PM - 8:30 PM', description: 'Closing and Rewarding' },
+    { time: '3:00 PM - 4:00 PM', description: 'Pizza & Code Networking' },
+    { time: '4:00 PM - 7:00 PM', description: 'Working on Projects Part 2' },
+    { time: '7:00 PM - 8:00 PM', description: 'Closing and Rewarding' },
   ];
 
   return (
