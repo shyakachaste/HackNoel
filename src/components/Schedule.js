b91abc036f6080d894ab4f6f25296fa1f949ed71
Shyaka Chaste <liochastej@gmail.com>
2024-10-11 10:36:48 -0700

    updated

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -4,15 +4,18 @@ import { FaClock } from 'react-icons/fa';
 
 const Schedule = () => {
   const events = [
-    { time: '7:15 AM - 8:00 AM', description: 'Welcoming' },
-    { time: '8:00 AM - 8:45 AM', description: 'Introducing HackNoel' },
-    { time: '8:45 AM - 9:15 AM', description: 'Breakfast' },
-    { time: '9:15 AM - 12:15 PM', description: 'Working on Projects' },
-    { time: '12:15 PM - 1:15 PM', description: 'Lunch' },
-    { time: '1:15 PM - 2:00 PM', description: 'Guest of Honor Speech' },
-    { time: '2:00 PM - 3:00 PM', description: 'Code Networking' },
-    { time: '3:00 PM - 6:00 PM', description: 'Working on Projects Part 2' },
-    { time: '6:00 PM - 7:00 PM', description: 'Closing and Rewarding' },
+    { time: '7:15 AM - 8:00 AM', description: 'Doors Open' },
+    { time: '8:00 AM - 8:30 AM', description: 'Opening Ceremony' },
+    { time: '8:30 AM - 9:00 AM', description: 'Breakfast' },
+    { time: '9:00 AM - 10:00 AM', description: 'Workshop' },
+    { time: '10:00 AM - 12:00 AM', description: 'Working on Projects Part 1' },
+    { time: '12:00 AM - 12:45 PM', description: 'Special Guest Speech' },
+    { time: '12:45 PM - 1:45 PM', description: 'Lunch' },
+    { time: '1:45 PM - 2:45 PM', description: 'Working on Projects Part 2' },
+    { time: '2:45 PM - 3:15 PM', description: 'Code Networking' },
+    { time: '3:15 PM - 3:45 PM', description: 'Speech from Organizers' },
+    { time: '3:45 PM - 5:15 PM', description: 'Project Presentations' },
+    { time: '5:15 PM - 6:15 PM', description: 'Judging,Awarding and Closing' },
   ];
 
   return (
