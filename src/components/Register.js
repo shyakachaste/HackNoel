fc25c0c72ff3ab6d33c9b56905b90b57bfd8c700
Shyaka Chaste <liochastej@gmail.com>
2024-10-21 19:00:19 +0200

    updated

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,7 +4,7 @@ import '../styles/Register.css';
 const Register = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
-      window.location.href = 'https://lu.ma/2dt6d5hc';
+      window.location.href = 'https://ed0azuo2uuh.typeform.com/to/Ljkvjssd';
     }, 4000);
 
     return () => clearTimeout(timer);
@@ -13,7 +13,7 @@ const Register = () => {
   return (
     <div className="register-container">
       <h1>Redirecting to Registration...</h1>
-      <p>If you are not redirected automatically, <a href="https://lu.ma/2dt6d5hc">click here</a>.</p>
+      <p>If you are not redirected automatically, <a href="https://ed0azuo2uuh.typeform.com/to/Ljkvjssd">click here</a>.</p>
       <div className="loader"></div>
     </div>
   );
