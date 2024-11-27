89236a0f1483c1f9a87843fad4fc305098aecee7
Shyaka Chaste <liochastej@gmail.com>
2024-11-27 16:54:18 +0200

    updated

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -4,17 +4,14 @@ import { FaClock } from 'react-icons/fa';
 
 const Schedule = () => {
   const events = [
-    { time: '7:15 AM - 8:00 AM', description: 'Doors Open' },
-    { time: '8:00 AM - 8:30 AM', description: 'Opening Ceremony' },
-    { time: '8:30 AM - 9:00 AM', description: 'Breakfast' },
-    { time: '9:00 AM - 10:00 AM', description: 'Web3 Development Workshop' },
-    { time: '10:00 AM - 12:30 AM', description: 'Working on Projects Part 1' },
-    { time: '12:30 AM - 1:15 PM', description: 'Special Guest Speech & Sponsor Showcase' },
-    { time: '1:15 PM - 2:15 PM', description: 'Lunch Break' },
-    { time: '2:15 PM - 3:15 PM', description: 'Working on Projects Part 2' },
-    { time: '3:15 PM - 3:30 PM', description: 'Code Networking' },
-    { time: '3:30 PM - 3:45 PM', description: 'Speech from Organizers' },
-    { time: '3:45 PM - 6:00 PM', description: 'Project Presentations & Judging' },
+    { time: '8:00 AM - 8:30 AM', description: 'Doors Open' },
+    { time: '8:30 AM - 9:30 AM', description: 'Opening Remarks & Breakfast' },
+    { time: '9:30 AM - 11:00 AM', description: 'Web3 Development Workshop' },
+    { time: '11:00 AM - 1:30 AM', description: 'Working on Projects Part 1' },
+    { time: '1:30 PM - 2:30 PM', description: 'Lunch Break' },
+    { time: '2:30 AM - 3:15 PM', description: 'Special Guest Speech & Sponsor Showcase' },
+    { time: '3:15 PM - 4:15 PM', description: 'Working on Projects Part 2 & Code Networking' },
+    { time: '4:15 PM - 6:00 PM', description: 'Project Presentations & Judging' },
     { time: '6:00 PM - 6:30 PM', description: 'Awarding Ceremony & Closing Remarks' },
   ];
 
