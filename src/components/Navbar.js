d97b773be0eed348f4a598b8bd5bf107dcfd2985
Shyaka Chaste <liochastej@gmail.com>
2024-12-20 22:21:25 +0200

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,8 +20,6 @@ const Navbar = () => {
       <div className={`navbar-menu ${isOpen ? 'open' : ''}`}>
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/gallery">Gallery</Link></li>
           <li>
             <a href="https://hcb.hackclub.com/donations/start/hack-noel" target="_blank" rel="noopener noreferrer">
               Donate
